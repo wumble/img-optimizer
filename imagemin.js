15c2dee4a8133679cf86243c637a239b9f7fae3e
Manuel Bartels <git@manuelbartels.de>
2018-08-19 16:36:33 +0200

    added editorconfig

--- imagemin.js
+++ imagemin.js
@@ -3,23 +3,23 @@ const mozjpeg = require('imagemin-mozjpeg');
 const pngquant = require('imagemin-pngquant');
 const svgo = require('imagemin-svgo');
 const del = require('del');
- 
+
 
 (async () => {
-	await del('./dist/**/*');
-	
-	const files = await imagemin(['src/**/*.{JPG,jpg,PNG,png,SVG,svg}'], 'dist/', {
-		plugins: [
-			mozjpeg({progressive: true, quality: 85}),
-			pngquant({speed: 6, quality: 80}),
-			svgo({
-				plugins: [
-					{removeViewBox: false},
-					{removeDimensions: true},
-					{removeStyleElement: true}
-				]
-			})
-		]
-	});
-	console.log(files);
+  await del('./dist/**/*');
+
+  const files = await imagemin(['src/**/*.{JPG,jpg,PNG,png,SVG,svg}'], 'dist/', {
+    plugins: [
+      mozjpeg({progressive: true, quality: 85}),
+      pngquant({speed: 6, quality: 80}),
+      svgo({
+        plugins: [
+          {removeViewBox: false},
+          {removeDimensions: true},
+          {removeStyleElement: true}
+        ]
+      })
+    ]
+  });
+  console.log(files);
 })();
