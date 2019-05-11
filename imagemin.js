8dbedf232d469fe2f890a57a1e000a9e92891cb6
Manuel Bartels <mail@manuelbartels.de>
2019-05-11 20:59:23 +0200

    updated pngquant read files recursive in folders

--- imagemin.js
+++ imagemin.js
@@ -3,23 +3,43 @@ const mozjpeg = require('imagemin-mozjpeg');
 const pngquant = require('imagemin-pngquant');
 const svgo = require('imagemin-svgo');
 const del = require('del');
+const fs = require('fs');
 
+
+const readFolders = (dir) => {
+  return new Promise((resolve) => {
+    const folders = [];
+    fs.readdir(dir, (error, files) => {
+      files.forEach((item) => {
+        if (fs.lstatSync(`${dir}${item}`).isDirectory()) folders.push(`${dir}${item}`);
+      });
+      resolve(folders);
+    });
+  });
+}
 
 (async () => {
   await del('./dist/**/*');
+  const files = [];
+  const folders = await readFolders('src/');
 
-  const files = await imagemin(['src/'], 'dist/', {
-    plugins: [
-      mozjpeg({progressive: true, quality: 85}),
-      pngquant({speed: 6, quality: 80}),
-      svgo({
-        plugins: [
-          {removeViewBox: false},
-          {removeDimensions: true},
-          {removeStyleElement: true}
-        ]
-      })
-    ]
+  folders.forEach(async (folder) => {
+    const distFolder = folder.replace('src/', '');
+    const tempFiles = await imagemin([folder], `dist/${distFolder}`, {
+      plugins: [
+        mozjpeg({progressive: true, quality: 85}),
+        pngquant({speed: 6, quality: [0.75, 0.90]}),
+        svgo({
+          plugins: [
+            {removeViewBox: false},
+            {removeDimensions: true},
+            {removeStyleElement: true}
+          ]
+        })
+      ]
+    });
+
+    files.push(tempFiles);
   });
-  console.log(files);
+  // console.log(tempFiles);
 })();
